5388d31d7f43b10bf6f76e3a0c2f5747a9e35cc7
Laura <lauradelpinodiaz94@gmail.com>
2017-03-27 19:03:18 +0200

    Right Labels

--- ioLBP.m
+++ ioLBP.m
@@ -2,14 +2,13 @@ function [files] = ioLBP(path,destinyPath)
 %Cargar todas las imágenes del directorio
 files = dir(path);
 
+
 %Para todas las imagenes del directorio
 for img = 3:size(files,1)
-    %pasar la imagen a gris
-    lbpImg = LBPu(rgb2gray(imread([path,files(img).name])))
-    
+    %pasar la imagen a gris y  a la función LBPu.
+    lbpImg = LBPu(rgb2gray(imread([path,files(img).name])));
+    imwrite(uint8(lbpImg),[destinyPath,'-LBP-',files(img).name]);
 end
 
-
-%pasarlo a la función LBPu.
 %guardar la imagen en el sistema de ficheros. 
 end
